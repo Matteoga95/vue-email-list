2727037d0e9df2d50779bd5d6c3c40203f63044f
matteoga95 <gallo.work95@gmail.com>
2022-11-08 14:29:35 +0100

    indirizzi stmapati a pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,20 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        return { 
+        return {
+            listaMail:[]
+        }
+    },
+    mounted() {
 
+        for (let index = 0; index < 10; index++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(response => {
+                    console.log(response);
+                    this.listaMail.push(response.data.response)
+                    console.log(  this.listaMail);
+                })
         }
     }
-  
 }).mount('#app');
